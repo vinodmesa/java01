9341295223264aa0739a7108d564efecfb421c97
Lenovo <Lenovo@vino.bbrouter>
2024-02-29 18:18:48 +0530

    task1 -- Dev

--- GitPro01/src/main/java/com/nt/main/AppTest.java
+++ GitPro01/src/main/java/com/nt/main/AppTest.java
@@ -1,10 +1,12 @@
 package com.nt.main;
-
+import com.nt.service.ArithmeticOperations;
 public class AppTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		ArithmeticOperations ar=new ArithmeticOperations();
+		 System.out.println("sum : "+ar.sum(100,200));
+		 System.out.println("sub : "+ar.sub(500,100));
 	}
 
 }
